174126d5533a245a90cc9692033e58e78af42828
jasmineaf22 <jasmineaf04@gmail.com>
2025-04-30 12:11:47 +0700

    add pert 5

---
+++ pertemuan 5 - lcd/src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+
+int SDA_pin = D7;
+int SCL_pin = D4;
+LiquidCrystal_I2C lcd(0x27, 16,2);
+
+void setup() {
+Wire.begin(SDA_pin, SCL_pin);
+lcd.init();
+lcd.backlight();
+lcd.setCursor(0, 0);
+lcd.print("NDX AKA FAMILIA");
+lcd.setCursor(0, 1);
+lcd.print("  -Desva");
+}
+
+void loop() {
+  lcd.scrollDisplayLeft();
+  delay(300);
+}
